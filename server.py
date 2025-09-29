f0f67a949ddd33f5beca1f928858e71aae8a6424
AlphaDesignGutierrez <138022477+AlphaDesignGutierrez@users.noreply.github.com>
2025-09-28 21:45:48 -0600

    Update server.py

--- server.py
+++ server.py
@@ -8,18 +8,24 @@ connected_clients = set()
 
 # Define the connection handler function
 async def handler(websocket):
+    uri = "wss://alpha-server.koyeb.app/"
     connected_clients.add(websocket)
-    async for message in websocket:
-        print(f"client: {message}")
-        websockets.broadcast(connected_clients, message)
+    try:
+        async with websockets.connect(uri) as websocket:
+            async for message in websocket:
+                print(f"client: {message}")
+                websockets.broadcast(connected_clients, message)
+    finally:
+        connected_clients.remove(websocket)
 
 # Set up and run the server
 async def main():
     async with websockets.serve(handler, HOST, PORT):
-        print("WebSocket server started on ws://0.0.0.0:8765")
+        print(f"WebSocket server started on {PORT}")
         await asyncio.Future()  # Run forever
 
 if __name__ == "__main__":
 
     asyncio.run(main())
+
 
